361920113cd4a14b069fd05fb5428b87d9d52447
Praveen <praveensingh55602@gmail.com>
2024-05-12 21:36:54 +0530

    deploy error fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,8 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off',
+  },
 }
